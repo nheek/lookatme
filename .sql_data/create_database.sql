0a6fdccdb2553a9907638577cf7fed622f859907
nheek <nickjameshipol@gmail.com>
2024-10-15 13:40:08 +0200

    implemented Kubernetes hopefully

--- .sql_data/create_database.sql
+++ .sql_data/create_database.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS lookatme.public."Counter" (
+CREATE TABLE IF NOT EXISTS "Counter" (
     id SERIAL PRIMARY KEY,
     looked_overall INT DEFAULT 0,
     tapped_overall INT DEFAULT 0
 );
 
-INSERT INTO lookatme.public."Counter" (looked_overall, tapped_overall)
+INSERT INTO "Counter" (looked_overall, tapped_overall)
 SELECT 0, 0
-WHERE NOT EXISTS (SELECT 1 FROM lookatme.public."Counter");
+WHERE NOT EXISTS (SELECT 1 FROM "Counter");
--- src/database.js
+++ src/database.js
@@ -3,11 +3,25 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const db = new Pool({
-  host: process.env.POSTGRES_HOST,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
-  port: parseInt(process.env.POSTGRES_PORT, 10),
+  host: process.env.DB_HOST,
+  port: 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
+
+// Test the connection
+const testConnection = async () => {
+  try {
+    const client = await db.connect();
+    console.log('Connected to PostgreSQL database successfully');
+    client.release(); // Release the client back to the pool
+  } catch (error) {
+    console.error('Error connecting to PostgreSQL database', error);
+  }
+};
+
+testConnection();
+
 
 module.exports = { db };
